241477acbfd0d4d6e092f1888df0107addc4f510
Mohammad Sahil Shaikh <sahil823794@gmail.com>
2025-03-20 19:58:27 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33ab";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggIn = 1;
+
+let booleanisLoggIn = Boolean(isLoggIn);
+
+console.log(booleanisLoggIn)
+
+//1 => true; 0 => false;
+// "" => false
+// "sahil" => true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
