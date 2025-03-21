8d9ec190479979156815018a2368f03327668fb0
Mohammad Sahil Shaikh <sahil823794@gmail.com>
2025-03-21 20:24:27 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = "33ab";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
 
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -18,7 +18,7 @@ let isLoggIn = 1;
 
 let booleanisLoggIn = Boolean(isLoggIn);
 
-console.log(booleanisLoggIn)
+// console.log(booleanisLoggIn)
 
 //1 => true; 0 => false;
 // "" => false
@@ -29,6 +29,52 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
+
+
+
+// **********************************operations******************************************
+
+let value = 3;
+
+let negValue = -value;
+
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+
+let str2 = "sahil";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+
+console.log(true)
+console.log(+true);
+console.log(+'');
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
+
+// link for ecmascript
+// https://tc39.es/ecma262/#sec-stringindexof
